a5b592e51cf008f04503c3808a1e76d76db6eb70
Akene-Uzezi <xtremejoexj@gmail.com>
2026-07-31 14:12:47 +0100

    update gitignore and add grpc server to orders

--- orders/grpc_handler.go
+++ orders/grpc_handler.go
@@ -1,7 +1,26 @@
 package main
 
-import pb "oms-common/api"
+import (
+	"context"
+	"log"
+	pb "oms-common/api"
+
+	"google.golang.org/grpc"
+)
 
 type grpcHandler struct {
 	pb.UnimplementedOrderServiceServer
+}
+
+func NewGrpcHandler(grpcServer *grpc.Server) {
+	handler := &grpcHandler{}
+	pb.RegisterOrderServiceServer(grpcServer, handler)
+}
+
+func (h *grpcHandler) CreateOrder(context.Context, *pb.CreateOrderRequest) (*pb.Order, error) {
+	log.Println("New Order Received")
+	o := &pb.Order{
+		ID: "42",
+	}
+	return o, nil
 }
