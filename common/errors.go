24ccb96640a5500d76677172e150975df7170589
Akene-Uzezi <xtremejoexj@gmail.com>
2026-07-31 16:08:01 +0100

    complete basic orders service

---
+++ common/errors.go
@@ -0,0 +1,5 @@
+package common
+
+import "errors"
+
+var ErrNoItems = errors.New("Items must have at least one item")
--- gateway/http_handler.go
+++ gateway/http_handler.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"errors"
 	"net/http"
 	common "oms-common"
 	pb "oms-common/api"
+
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 type handler struct {
@@ -27,8 +31,42 @@ func (h *handler) HandleCreateOrder(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	h.client.CreateOrder(r.Context(), &pb.CreateOrderRequest{
+	if err := validateItems(items); err != nil {
+		common.WriteError(w, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	o, err := h.client.CreateOrder(r.Context(), &pb.CreateOrderRequest{
 		CustomerID: customerID,
 		Items:      items,
 	})
+	rStatus := status.Convert(err)
+	if rStatus != nil {
+		if rStatus.Code() != codes.InvalidArgument {
+			common.WriteError(w, http.StatusBadRequest, rStatus.Message())
+			return
+		}
+		common.WriteError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	common.WriteJSON(w, http.StatusOK, o)
+}
+
+func validateItems(items []*pb.ItemsWithQuantity) error {
+	if len(items) == 0 {
+		return common.ErrNoItems
+	}
+
+	for _, i := range items {
+		if i.ID == "" {
+			return errors.New("item ID is required")
+		}
+
+		if i.Quantity <= 0 {
+			return errors.New("items must have a valid quantity")
+		}
+	}
+
+	return nil
 }
